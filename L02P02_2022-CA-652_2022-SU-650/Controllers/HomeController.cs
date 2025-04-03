7363e70ff8e137ad1d6d169624af5ddc88afa30b
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-03 17:57:31 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022-CA-652_2022-SU-650/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using L02P02_2022_CA_652_2022_SU_650.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022_CA_652_2022_SU_650.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02P02_2022-CA-652_2022-SU-650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022_CA_652_2022_SU_650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
