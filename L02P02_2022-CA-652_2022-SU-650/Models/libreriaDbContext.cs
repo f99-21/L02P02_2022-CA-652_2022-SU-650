5393c5bdb60618b8e65bce768149f255880cdbbd
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-04-03 18:31:59 -0600

    Crear DbSets

--- L02P02_2022-CA-652_2022-SU-650/Models/libreriaDbContext.cs
+++ L02P02_2022-CA-652_2022-SU-650/Models/libreriaDbContext.cs
@@ -7,5 +7,13 @@ namespace L02P02_2022_CA_652_2022_SU_650.Models
         public libreriaDbContext(DbContextOptions options) : base(options) 
         {
         }
+
+        public DbSet<clientes> clientes { get; set; }
+        public DbSet<autores> autores { get; set; }
+        public DbSet<categorias> categorias { get; set; }
+        public DbSet<libros> libros { get; set; }
+        public DbSet<comentarios_libros> comentarios_libros { get; set; }
+        public DbSet<pedido_encabezado> pedido_encabezado { get; set; }
+        public DbSet<pedido_detalle> pedido_detalle { get; set; }
     }
 }
