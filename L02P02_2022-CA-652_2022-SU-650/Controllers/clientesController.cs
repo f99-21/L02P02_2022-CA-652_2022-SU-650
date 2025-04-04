3f420fe668fdf5aa380147085725f6afb814ccca
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-04-03 19:25:21 -0600

    Crear controlador de clientes y formulario de ingreso de clientes

---
+++ L02P02_2022-CA-652_2022-SU-650/Controllers/clientesController.cs
@@ -0,0 +1,44 @@
+﻿using L02P02_2022_CA_652_2022_SU_650.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022_CA_652_2022_SU_650.Controllers
+{
+    public class clientesController : Controller
+    {
+        private readonly libreriaDbContext _context;
+
+        public clientesController(libreriaDbContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("agregarCliente");
+        }
+
+        public IActionResult agregarCliente() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult agregarCliente(string nombre, string apellido, string email, string direccion) 
+        {
+            var nuevoCliente = new clientes
+            {
+                nombre = nombre,
+                apellido = apellido,
+                email = email,
+                direccion = direccion,
+                created_at = DateTime.Now
+            };
+
+            _context.clientes.Add(nuevoCliente);
+            _context.SaveChanges();
+
+            //Cambiar a controlador de pedidos
+            return View();
+        }
+    }
+}
