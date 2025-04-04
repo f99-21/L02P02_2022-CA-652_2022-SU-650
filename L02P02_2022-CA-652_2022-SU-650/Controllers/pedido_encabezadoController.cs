24e1878509a06bc6582850740bb224e44b486f3f
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-03 19:23:53 -0600

    controllador pedido

---
+++ L02P02_2022-CA-652_2022-SU-650/Controllers/pedido_encabezadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022_CA_652_2022_SU_650.Controllers
+{
+    public class pedido_encabezadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
